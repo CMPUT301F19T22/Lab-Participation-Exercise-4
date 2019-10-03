95b740c42e7cb52e46fe4247de7f883c6d78f53a
WJXHenry <wjxhenry@gmail.com>
2019-10-02 18:18:57 -0600

    Fix merge conflicts

--- app/src/main/java/com/wjxhenrydevgmail/labparticipationexercise4/Shape.java
+++ app/src/main/java/com/wjxhenrydevgmail/labparticipationexercise4/Shape.java
@@ -3,10 +3,5 @@ package com.wjxhenrydevgmail.labparticipationexercise4;
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< HEAD
-
-    String color = "blue";
-=======
     String color = "red";
->>>>>>> d2e92a5032b79503089a187977c41f5952a10ec8
 }
